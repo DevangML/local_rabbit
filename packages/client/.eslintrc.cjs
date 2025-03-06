5f88463e30773b708d7b9cfc2188c892e8ec8cbf
Devang <devang.manjramkar@elastic.run>
2025-03-06 22:51:26 +0530

    chasnges

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -3,15 +3,14 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true,
+    node: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
@@ -20,32 +19,95 @@ module.exports = {
   settings: {
     react: {
       version: '18.2'
-    },
+    }
   },
   plugins: ['react-refresh'],
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    '*.config.js',
+    '*.config.ts',
+    'public/**',
+    'coverage/**',
+    'build/**'
+  ],
   globals: {
-    console: 'readonly',
-    document: 'readonly',
+    // Browser globals
     window: 'readonly',
+    document: 'readonly',
+    navigator: 'readonly',
+    console: 'readonly',
     fetch: 'readonly',
-    Promise: 'readonly',
+    localStorage: 'readonly',
+    sessionStorage: 'readonly',
     setTimeout: 'readonly',
     clearTimeout: 'readonly',
     setInterval: 'readonly',
     clearInterval: 'readonly',
-    localStorage: 'readonly',
-    sessionStorage: 'readonly',
+
+    // Web API globals
+    URL: 'readonly',
+    File: 'readonly',
+    FileReader: 'readonly',
+    Blob: 'readonly',
+    Image: 'readonly',
+    Event: 'readonly',
+    HTMLElement: 'readonly',
+    ImageData: 'readonly',
+    Worker: 'readonly',
+    Response: 'readonly',
+    performance: 'readonly',
+
+    // Node.js globals
     process: 'readonly',
+    global: 'readonly',
     module: 'writable',
+    require: 'readonly',
+    __dirname: 'readonly',
+
+    // Custom globals
+    confirm: 'readonly',
+    Promise: 'readonly'
   },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     'react/prop-types': 'off',
     'no-unused-vars': 'warn',
-    'no-undef': 'warn',
-    '@typescript-eslint/no-unused-vars': 'off'
+    'no-undef': 'off',
+    'react-hooks/exhaustive-deps': 'warn'
   },
+  overrides: [
+    {
+      files: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx', '**/tests/**'],
+      env: {
+        jest: true
+      },
+      globals: {
+        describe: 'readonly',
+        test: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        vi: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        jest: 'readonly',
+        afterAll: 'readonly',
+        beforeAll: 'readonly'
+      }
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended'
+      ],
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off'
+      }
+    }
+  ]
 } 
