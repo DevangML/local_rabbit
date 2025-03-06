8ce896926617b6bbeee8524cb2c429b216b4482f
Devang <devang.manjramkar@elastic.run>
2025-03-06 22:48:50 +0530

    changes

--- packages/client/.eslintrc.cjs
+++ packages/client/.eslintrc.cjs
@@ -1,107 +1,51 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
     es2020: true,
     node: true,
-    jest: true,
-    worker: true
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'plugin:@typescript-eslint/recommended'
   ],
-  ignorePatterns: [
-    'dist',
-    '.eslintrc.js',
-    '.eslintrc.cjs',
-    'public/sw.js',
-    'node_modules',
-    'scripts',
-    'build',
-    'coverage',
-    'vite.config.js',
-    'vite.config.ts',
-    '*.test.js',
-    '*.test.jsx',
-    '*.test.ts',
-    '*.test.tsx',
-    'tests'
-  ],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true
-    }
+    project: null
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: '18.2'
+    },
   },
+  plugins: ['react-refresh'],
   globals: {
-    window: 'readonly',
-    document: 'readonly',
     console: 'readonly',
+    document: 'readonly',
+    window: 'readonly',
     fetch: 'readonly',
+    Promise: 'readonly',
     setTimeout: 'readonly',
     clearTimeout: 'readonly',
     setInterval: 'readonly',
     clearInterval: 'readonly',
     localStorage: 'readonly',
-    URL: 'readonly',
-    HTMLElement: 'readonly',
+    sessionStorage: 'readonly',
     process: 'readonly',
-    __dirname: 'readonly',
-    global: 'readonly',
     module: 'writable',
-    require: 'readonly',
-    exports: 'readonly',
-    Event: 'readonly',
-    FileReader: 'readonly',
-    Blob: 'readonly',
-    Worker: 'readonly',
-    ImageData: 'readonly',
-    Response: 'readonly',
-    Image: 'readonly',
-    File: 'readonly',
-    performance: 'readonly',
-    confirm: 'readonly',
-    self: 'readonly',
-    caches: 'readonly',
-    describe: 'readonly',
-    test: 'readonly',
-    expect: 'readonly',
-    beforeEach: 'readonly',
-    afterEach: 'readonly',
-    vi: 'readonly'
   },
   rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
     'react/prop-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-    'no-undef': 'off'
+    'no-unused-vars': 'warn',
+    'no-undef': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off'
   },
-  overrides: [
-    {
-      files: ['**/*.test.js', '**/*.test.jsx', '**/*.test.ts', '**/*.test.tsx'],
-      env: {
-        jest: true
-      }
-    },
-    {
-      files: ['**/scripts/**'],
-      env: {
-        node: true
-      }
-    }
-  ]
-}; 
+} 
