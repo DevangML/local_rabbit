5fcaedc0dd72ad61f62ed2f29b85e3c34c5f00c6
Devang <devang.manjramkar@elastic.run>
2025-03-09 00:08:40 +0530

    changes

--- packages/server/dev.mjs
+++ packages/server/dev.mjs
@@ -4,6 +4,9 @@
 import { register } from 'node:module';
 import { pathToFileURL } from 'node:url';
 
+// Set NODE_ENV to development
+process.env.NODE_ENV = 'development';
+
 // Register the TypeScript loader
 register('ts-node/esm', pathToFileURL('./'));
 
